c1049c352e4fae9aed3dafcd3f305d8ecfcada14
stillnight88 <dnight2112@gmail.com>
2026-03-12 17:33:07 +0530

    refactor(db): add centralized model exports

---
+++ lib/db/models/index.ts
@@ -0,0 +1,13 @@
+// Centralized export for all database models 
+
+// Core Identity Models
+export { UserModel } from './user.model';
+export { RefreshTokenModel } from './refresh-token.model';
+export { OTPModel } from './otp.model';
+
+// Vendor System Models
+export { VendorApplicationModel } from './vendor-application.model';
+export { VendorProfileModel } from './vendor-profile.model';
+
+// Audit System
+export { AuditLogModel } from './audit-log.model';
